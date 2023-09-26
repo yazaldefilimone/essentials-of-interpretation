75c617513628f162799cee189f79ebb6bacaf415
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-09-27 00:32:33 +0200

    *

--- src/environment.js
+++ src/environment.js
@@ -1,19 +1,28 @@
-
 class Environment {
-  constructor(record = new Map()) {
-    this.record = record
-  }
-  define(name, value) {
-    this.record.set(name, value)
-    return value
-  }
-  lookup(name) {
-    if (!this.record.has(name)) {
-      throw ReferenceError(`Variable ${name} is not defined!`)
-    }
-    return this.record.get(name)
-  }
+	constructor(record = new Map(), parent = null) {
+		this.record = record;
+		this.parent = parent;
+	}
+	define(name, value) {
+		this.record.set(name, value);
+		return value;
+	}
+	lookup(name) {
+		return this.resolve(name).record.get(name);
+	}
+	assign(name, value) {
+		this.resolve(name).record.set(name, value);
+	}
+
+	resolve(name) {
+		if (this.record.has(name)) {
+			return this;
+		}
+		if (this.parent !== null) {
+			return this.parent.resolve(name);
+		}
+		throw ReferenceError(`Variable ${name} is not defined!`);
+	}
 }
 
-
-module.exports = Environment
+module.exports = Environment;
--- src/evaluator.js
+++ src/evaluator.js
@@ -1,89 +1,140 @@
-const assert = require('assert')
-const fs = require('fs')
-const path = require('path')
-const Environment = require('./environment')
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const Environment = require('./environment');
+
 class Evaluator {
-  constructor(globalEnv = new Environment()) {
-    this.globalEnv = globalEnv
-  }
+	constructor(globalEnv = new Environment()) {
+		this.globalEnv = globalEnv;
+	}
 
-  eva(exp, env = this.globalEnv) {
-    if (isNumber(exp)) {
-      return exp
-    }
+	eva(exp, env = this.globalEnv) {
+		if (isNumber(exp)) {
+			return exp;
+		}
 
-    if (isString(exp)) {
-      return exp.slice(1, -1)
-    }
+		if (isString(exp)) {
+			return exp.slice(1, -1);
+		}
 
-    if (exp[0] === '+') {
-      return this.eva(exp[1]) + this.eva(exp[2])
-    }
-    if (exp[0] === '*') {
-      return this.eva(exp[1]) * this.eva(exp[2])
-    }
+		if (exp[0] === '+') {
+			return this.eva(exp[1], env) + this.eva(exp[2], env);
+		}
+		if (exp[0] === '*') {
+			return this.eva(exp[1], env) * this.eva(exp[2], env);
+		}
+		// Block: sequence of expressions
+		if (exp[0] === 'begin') {
+			const parentEnv = new Environment(new Map(), env);
+			return this._evalBlock(exp, parentEnv);
+		}
 
+		// assign declaration
+		if (exp[0] === 'set') {
+			const [_, name, value] = exp;
+			return env.assign(name, this.eva(value, env));
+		}
+		// Variable declaration
+		if (exp[0] === 'var') {
+			const [_, name, value] = exp;
+			return env.define(name, this.eva(value, env));
+		}
 
-    if (exp[0] === 'var') {
-      const [_, name, value] = exp
-      return env.define(name, this.eva(value))
-    }
+		if (isVariableName(exp)) {
+			return env.lookup(exp);
+		}
 
-    if (isVariableName(exp)) {
-      return env.lookup(exp)
-    }
+		throw `Unimplemented: ${JSON.stringify(exp)}`;
+	}
 
-
-
-    throw `Unimplemented: ${JSON.stringify(exp)}`
-  }
+	_evalBlock(exp, env) {
+		const [_tag, ...expressions] = exp;
+		return expressions.reduce((_, exp) => this.eva(exp, env), null);
+	}
 }
 
-
-
 function isNumber(exp) {
-  return typeof exp === 'number'
+	return typeof exp === 'number';
 }
 
 function isString(exp) {
-  return typeof exp === 'string' && exp.at(0) == '"' && exp.at(-1) == '"'
+	return typeof exp === 'string' && exp.at(0) == '"' && exp.at(-1) == '"';
 }
 
 function isVariableName(exp) {
-  return typeof exp === 'string' && /^[+\-*/<>=a-zA-Z0-9_]+$/.test(exp);
+	return typeof exp === 'string' && /^[+\-*/<>=a-zA-Z0-9_]+$/.test(exp);
 }
 
 // Tests
-const package = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), { encoding: 'utf-8' }))
+const package = JSON.parse(
+	fs.readFileSync(path.resolve(__dirname, '../package.json'), {
+		encoding: 'utf-8',
+	}),
+);
 const internalEnv = [
-  ['null', null],
-  ['true', true],
-  ['false', false],
-  ['VERSION', package.version]
-]
-const table = new Map(internalEnv)
-const environment = new Environment(table)
-const evaluator = new Evaluator(environment)
+	['null', null],
+	['true', true],
+	['false', false],
+	['VERSION', package.version],
+];
+const table = new Map(internalEnv);
+const environment = new Environment(table);
+const evaluator = new Evaluator(environment);
 // Number
-assert.strictEqual(evaluator.eva(1), 1)
+assert.strictEqual(evaluator.eva(1), 1);
 // String
-assert.strictEqual(evaluator.eva('"Hello"'), 'Hello')
+assert.strictEqual(evaluator.eva('"Hello"'), 'Hello');
 
 // Adicional Operator
-assert.strictEqual(evaluator.eva(['+', 2, 2]), 4)
-assert.strictEqual(evaluator.eva(['+', ["+", 1, 2], 2]), 5)
+assert.strictEqual(evaluator.eva(['+', 2, 2]), 4);
+assert.strictEqual(evaluator.eva(['+', ['+', 1, 2], 2]), 5);
 // Multiplication
-assert.strictEqual(evaluator.eva(['*', ["*", 2, 2], 2]), 8)
+assert.strictEqual(evaluator.eva(['*', ['*', 2, 2], 2]), 8);
 //
-assert.strictEqual(evaluator.eva(['*', ["+", 1, 2], 2]), 6)
+assert.strictEqual(evaluator.eva(['*', ['+', 1, 2], 2]), 6);
 
 // variable
-assert.strictEqual(evaluator.eva(['var', 'x', 10]), 10)
-assert.strictEqual(evaluator.eva('x'), 10)
-assert.strictEqual(evaluator.eva(['var', 'x', ['*', ["+", 1, 2], 2]]), 6)
-assert.strictEqual(evaluator.eva('VERSION'), package.version)
-assert.strictEqual(evaluator.eva(['var', 'isTrue', 'true']), true)
+assert.strictEqual(evaluator.eva(['var', 'x', 10]), 10);
+assert.strictEqual(evaluator.eva('x'), 10);
+assert.strictEqual(evaluator.eva(['var', 'x', ['*', ['+', 1, 2], 2]]), 6);
+assert.strictEqual(evaluator.eva('VERSION'), package.version);
+assert.strictEqual(evaluator.eva(['var', 'isTrue', 'true']), true);
 
+//  blocks
 
+assert.strictEqual(
+	evaluator.eva(['begin', ['var', 'x', 10], ['var', 'y', 20], ['+', ['*', 'x', 'y'], 30]]),
+	230,
+);
 
-console.log("All tests passed!")
+assert.strictEqual(
+	evaluator.eva(['begin', ['var', 'x', 10], ['begin', ['var', 'x', 20], 'x'], 'x']),
+	10,
+);
+
+assert.strictEqual(
+	evaluator.eva([
+		'begin',
+		// main scop
+		['var', 'y', 2],
+		[
+			'var',
+			'b',
+			[
+				'begin',
+				// second scop
+				['var', 'x', ['+', 'y', 10]],
+				'x',
+			],
+		],
+		'b',
+	]),
+	12,
+);
+// assign
+assert.strictEqual(
+	evaluator.eva(['begin', ['var', 'x', 10], ['begin', ['set', 'x', 20], 'x'], 'x']),
+	20,
+);
+
+console.log('All tests passed!');
