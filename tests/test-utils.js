85fa16e0fbf99ae228e5b419fc601bbe2015e7a7
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-04 16:56:40 +0200

    *

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -1,5 +1,7 @@
 
 const assert = require('assert')
+const evalParser = require('../src/parser')
+
 const test = function(evaluator, code, expected) {
   const exp = evalParser.parse(code)
   assert.strictEqual(evaluator.eva(exp), expected)
