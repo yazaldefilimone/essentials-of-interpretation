2923b73e92398178c5896a12f48a9196fc279637
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-07 21:52:52 +0200

    *

--- src/transformer.js
+++ src/transformer.js
@@ -1,8 +1,25 @@
 // JIT - transpile
 class Transformer {
-	transformDefToLambda(defExp) {
+	defToLambda(defExp) {
 		const [_tag, name, params, body] = defExp;
 		return ['var', name, ['lambda', params, body]];
+	}
+
+	switchToIf(switchExp) {
+		const [_tag, ...cases] = switchExp;
+		const ifExp = ['if', null, null, null];
+		let currentIfExp = ifExp;
+		for (let index = 0; index < cases.length - 1; index++) {
+			const [currentCase, currentBlock] = cases[index];
+			currentIfExp[1] = currentCase;
+			currentIfExp[2] = currentBlock;
+			// next case
+			const next = cases[index + 1];
+			const [nextCase, nextBlock] = next;
+			currentIfExp[3] = nextCase === 'else' ? nextBlock : ['if'];
+			currentIfExp = currentIfExp[3];
+		}
+		return ifExp;
 	}
 }
 
