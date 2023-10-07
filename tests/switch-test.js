57c102ff4443e834f14a7e70ead67f68777ca90c
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-07 21:52:57 +0200

    *

--- tests/switch-test.js
+++ tests/switch-test.js
@@ -1,7 +1,30 @@
 const test = require('./test-utils');
 
 module.exports = (evaluator) => {
-	// const code = `
-	// `;
-	// test(evaluator, code, 10);
+	const code = `
+  (begin
+    (var x 10)
+    (switch 
+      ((<= x  10) 100)
+      ((> x 10) 1000)
+      (else 50)
+    )
+  )
+	`;
+	test(evaluator, code, 100);
+
+	const codeStore = `
+  (begin
+    (var x 10)
+    (var result 
+      (switch 
+      ((<= x  10) 100)
+      ((> x 10) 1000)
+      (else 50)
+      )
+    )
+    result
+  )
+	`;
+	test(evaluator, codeStore, 100);
 };
