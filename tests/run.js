64cf540a3be274b94b92701ecc8a1f48b2bcf9e1
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-07 21:53:10 +0200

    *

--- tests/run.js
+++ tests/run.js
@@ -4,7 +4,7 @@ const exclude = ['run.js', 'test-utils.js'];
 const Evaluator = require('../src/evaluator');
 const bootstrap = async () => {
 	const all = await fs.readdir(path.resolve(__dirname));
-	const testers = all.filter((file) => !exclude.includes(file));
+	const testers = all.filter((file) => file.split('-').at(-1).includes('test'));
 	const requires = testers.map((testPath) => require(path.resolve(__dirname, testPath)));
 	const evaluator = new Evaluator();
 	requires.forEach((test) => test(evaluator));
