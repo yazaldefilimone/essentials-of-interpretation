b9a3b1600f1a6cb70ac0d074df58d406a7d13711
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-07 17:58:23 +0200

    *

--- tests/user-define-function-test.js
+++ tests/user-define-function-test.js
@@ -11,6 +11,8 @@ module.exports = (evaluator) => {
     (square 20)
   )
   `
+  test(evaluator,code, 20 * 20)
+  // closure
     const codeClosure =  `(begin
       (var global 10)
       (def calc (x y)
@@ -28,6 +30,21 @@ module.exports = (evaluator) => {
       result
   )
   `
-  test(evaluator,code, 20 * 20)
   test(evaluator,codeClosure, 10 + 5 + 2 + 10)
+
+  // recursion
+  const recursion =  `(begin
+   (def recursion (x)
+      (if (= x 1)
+        1
+        (* x  (recursion (- x 1)))
+      )
+   )
+   (recursion 5)
+  )
+  `
+  console.time('recursion')
+  test(evaluator,recursion, 120)
+  console.timeEnd('recursion')
+
 };
