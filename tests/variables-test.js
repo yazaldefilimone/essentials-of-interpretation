cdfd5e57eafb97a28ef27fa27340dbefd55878a6
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-12 23:54:44 +0200

    *

--- tests/variables-test.js
+++ tests/variables-test.js
@@ -3,19 +3,14 @@ const package = require('../package.json');
 
 module.exports = (evaluator) => {
 	// variable
-	assert.strictEqual(evaluator.eva(['var', 'x', 10]), 10);
-	assert.strictEqual(evaluator.eva('x'), 10);
-	assert.strictEqual(evaluator.eva(['var', 'x', ['*', ['+', 1, 2], 2]]), 6);
-	assert.strictEqual(evaluator.eva('VERSION'), package.version);
-	assert.strictEqual(evaluator.eva(['var', 'isTrue', 'true']), true);
+	assert.strictEqual(evaluator.eval(['var', 'x', 10]), 10);
+	assert.strictEqual(evaluator.eval('x'), 10);
+	assert.strictEqual(evaluator.eval(['var', 'x', ['*', ['+', 1, 2], 2]]), 6);
+	assert.strictEqual(evaluator.eval('VERSION'), package.version);
+	assert.strictEqual(evaluator.eval(['var', 'isTrue', 'true']), true);
 	// assign
 	assert.strictEqual(
-		evaluator.eva(['begin', 
-    ['var', 'x', 10], 
-    ['begin', 
-    ['set', 'x', 20], 'x'], 
-    'x']
-    ),
+		evaluator.eval(['begin', ['var', 'x', 10], ['begin', ['set', 'x', 20], 'x'], 'x']),
 		20,
 	);
 };
