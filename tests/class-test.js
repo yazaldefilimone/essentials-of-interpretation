a10ecebc90c548b7f7b48bf092e5bc7cfb50b49f
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-15 19:18:46 +0200

    *

--- tests/class-test.js
+++ tests/class-test.js
@@ -20,29 +20,19 @@ module.exports = (evaluator) => {
 	`;
 	test(evaluator, code, 30);
 	const codeCaseTwo = `
-     (class Point null
-      (begin
-        (def constructor (this x y)
-          (begin
-            (set (prop this x) x)
-            (set (prop this y) y)
-          )
-        )
-        (def calc (this)
-          (+ (prop this x) (prop this y))
-        )
-      )
-    )
     (class Point3D Point
       (begin
-        (def constructor (this x y z)
+        (def constructor (this x y z) 
+
           (begin
-            ((prop (super Point3D) constructor) this x y)
+                    ((prop (super Point3D) constructor) this x y)
             (set (prop this z) z)))
 
         (def calc (this)
           (+ ((prop (super Point3D) calc) this)
-             (prop this z)))))
+             (prop this z)))
+      )
+    )
 
     (var p (new Point3D 20 20 30))
 
