6a058505ccbd13436d37c5812febf7b668eed89a
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-15 21:59:23 +0200

    *

--- tests/run.js
+++ tests/run.js
@@ -1,11 +1,11 @@
 const fs = require('fs/promises');
 const path = require('path');
 const Evaluator = require('../src/evaluator');
+const evaluator = new Evaluator();
 const bootstrap = async () => {
-	const all = await fs.readdir(path.resolve(__dirname));
-	const testers = all.filter((file) => file.split('-').at(-1).includes('test'));
-	const requires = testers.map((testPath) => require(path.resolve(__dirname, testPath)));
-	const evaluator = new Evaluator();
+	const files = await fs.readdir(path.resolve(__dirname));
+	const testsPaths = files.filter((file) => file.split('-').at(-1).includes('test'));
+	const requires = testsPaths.map((testPath) => require(path.resolve(__dirname, testPath)));
 	requires.forEach((test) => test(evaluator));
 	evaluator.eval(['print', '"Hello,"', '"World!"']);
 	console.log('All tests passed!');
