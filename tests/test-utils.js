56ae0f5b37c3d42683c14e0ebd04d91972065686
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-15 21:59:28 +0200

    *

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
 const evalParser = require('../src/parser');
 
-const test = function (evaluator, code, expected) {
+function test(evaluator, code, expected) {
 	const exp = evalParser.parse(`(begin ${code})`);
 	assert.strictEqual(evaluator.evalGlobal(exp), expected);
-};
+}
 module.exports = test;
